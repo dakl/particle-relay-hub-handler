6f6f1f25fd14a7a11d5c29d7025922cba2520d8e
Daniel Klevebring <daniel.klevebring@gmail.com>
2019-01-20 20:27:02 +0100

    move logsetup to init

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,15 @@
-from app.config import Config
-from app.log import setup_logging
+import logging
 
-setup_logging()
+from app.config import Config
+
+
+def setup_logging():
+    if config.DEBUG:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+    logging.basicConfig(level=level)
+
 
 config = Config()
+setup_logging()
